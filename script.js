3d636330708180054f7ce3aca69f5cb7d4674069
arhan5353 <serefarhan@hotmail.com>
2026-01-28 00:55:56 +0300

    Fetch periodic table data from API

--- script.js
+++ script.js
@@ -1,76 +1,46 @@
-const elements = [
-  {
-    number: 1,
-    symbol: "H",
-    name: "Hidrojen",
-    type: "Ametal",
-    weight: "1.008",
-    electron: "1s¹",
-    melting: "-259 °C",
-    boiling: "-253 °C",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Hydrogen_discharge_tube.jpg/320px-Hydrogen_discharge_tube.jpg",
-    row: 1,
-    column: 1
-  },
-  {
-    number: 2,
-    symbol: "He",
-    name: "Helyum",
-    type: "Soygaz",
-    weight: "4.0026",
-    electron: "1s²",
-    melting: "-272 °C",
-    boiling: "-269 °C",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Helium_discharge_tube.jpg/320px-Helium_discharge_tube.jpg",
-    row: 1,
-    column: 18
-  },
-  {
-    number: 6,
-    symbol: "C",
-    name: "Karbon",
-    type: "Ametal",
-    weight: "12.011",
-    electron: "1s² 2s² 2p²",
-    melting: "3550 °C",
-    boiling: "4827 °C",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/GraphiteUSGOV.jpg/320px-GraphiteUSGOV.jpg",
-    row: 2,
-    column: 14
-  },
-  {
-    number: 26,
-    symbol: "Fe",
-    name: "Demir",
-    type: "Metal",
-    weight: "55.845",
-    electron: "[Ar] 3d⁶ 4s²",
-    melting: "1538 °C",
-    boiling: "2862 °C",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Iron_electrolytic_and_1cm3_cube.jpg/320px-Iron_electrolytic_and_1cm3_cube.jpg",
-    row: 4,
-    column: 8
-  }
-];
-
 const table = document.getElementById("periodicTable");
 const searchInput = document.getElementById("searchInput");
 
+let elements = [];
+
+fetch("https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json")
+  .then(res => res.json())
+  .then(data => {
+    elements = data.elements.map(e => ({
+      number: e.number,
+      symbol: e.symbol,
+      name: e.name,
+      type: translateType(e.category),
+      weight: e.atomic_mass,
+      electron: e.electron_configuration,
+      melting: e.melt ? e.melt + " K" : "Bilinmiyor",
+      boiling: e.boil ? e.boil + " K" : "Bilinmiyor",
+      image: `https://images-of-elements.com/${e.name.toLowerCase()}.jpg`,
+      row: e.ypos,
+      column: e.xpos
+    }));
+    drawTable(elements);
+  });
+
+function translateType(cat) {
+  if (cat.includes("metal") && !cat.includes("metalloid")) return "Metal";
+  if (cat.includes("metalloid")) return "Yarı Metal";
+  if (cat.includes("noble")) return "Soygaz";
+  return "Ametal";
+}
+
 function drawTable(list) {
   table.innerHTML = "";
-
   list.forEach(el => {
     const div = document.createElement("div");
     div.className = `element ${el.type.replace(" ", "")}`;
     div.style.gridRow = el.row;
     div.style.gridColumn = el.column;
-
     div.innerHTML = `
       <div class="number">${el.number}</div>
       <div class="symbol">${el.symbol}</div>
       <div class="name">${el.name}</div>
     `;
-
     div.onclick = () => openModal(el);
     table.appendChild(div);
   });
@@ -80,9 +50,7 @@ function openModal(el) {
   document.getElementById("modal").style.display = "flex";
   document.getElementById("modalName").innerText = `${el.name} (${el.symbol})`;
   document.getElementById("modalImage").src = el.image;
-
   document.getElementById("modalDetails").innerHTML = `
-    <p><b>Atom Numarası:</b> ${el.number}</p>
     <p><b>Atom Ağırlığı:</b> ${el.weight}</p>
     <p><b>Elektron Dizilimi:</b> ${el.electron}</p>
     <p><b>Erime Noktası:</b> ${el.melting}</p>
@@ -96,23 +64,14 @@ function closeModal() {
 }
 
 function filterElements(type) {
-  if (type === "all") {
-    drawTable(elements);
-  } else {
-    drawTable(elements.filter(e => e.type === type));
-  }
+  drawTable(type === "all" ? elements : elements.filter(e => e.type === type));
 }
 
 searchInput.addEventListener("input", () => {
-  const value = searchInput.value.toLowerCase();
-  drawTable(
-    elements.filter(e =>
-      e.name.toLowerCase().includes(value) ||
-      e.symbol.toLowerCase().includes(value) ||
-      e.number.toString().includes(value)
-    )
-  );
+  const v = searchInput.value.toLowerCase();
+  drawTable(elements.filter(e =>
+    e.name.toLowerCase().includes(v) ||
+    e.symbol.toLowerCase().includes(v) ||
+    e.number.toString().includes(v)
+  ));
 });
-
-drawTable(elements);
-
