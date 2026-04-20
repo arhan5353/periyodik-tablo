cd695c49d4bf6986a3064ff0b44ba66d0c7a147c
arhan5353 <serefarhan@hotmail.com>
2026-04-20 07:02:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,25 +3,33 @@ const arama = document.getElementById("searchInput");
 
 let elementler = [];
 
-/* 118 ELEMENT TÜRKÇE LİSTE */
-const turkceListe = {
-1:"Hidrojen",2:"Helyum",3:"Lityum",4:"Berilyum",5:"Bor",6:"Karbon",7:"Azot",8:"Oksijen",9:"Flor",10:"Neon",
-11:"Sodyum",12:"Magnezyum",13:"Alüminyum",14:"Silisyum",15:"Fosfor",16:"Kükürt",17:"Klor",18:"Argon",
-19:"Potasyum",20:"Kalsiyum",21:"Skandiyum",22:"Titanyum",23:"Vanadyum",24:"Krom",25:"Manganez",26:"Demir",27:"Kobalt",28:"Nikel",
-29:"Bakır",30:"Çinko",31:"Galyum",32:"Germanyum",33:"Arsenik",34:"Selenyum",35:"Brom",36:"Kripton",
-37:"Rubidyum",38:"Stronsiyum",39:"İtriyum",40:"Zirkonyum",41:"Niyobyum",42:"Molibden",43:"Teknesyum",44:"Rutenyum",
-45:"Rodyum",46:"Paladyum",47:"Gümüş",48:"Kadmiyum",49:"İndiyum",50:"Kalay",51:"Antimon",52:"Tellür",53:"İyot",54:"Ksenon",
-55:"Sezyum",56:"Baryum",57:"Lantan",58:"Seryum",59:"Praseodimyum",60:"Neodimyum",61:"Prometyum",62:"Samaryum",
-63:"Evropiyum",64:"Gadolinyum",65:"Terbiyum",66:"Disprosyum",67:"Holmiyum",68:"Erbiyum",69:"Tulyum",70:"İterbiyum",71:"Lutesyum",
-72:"Hafniyum",73:"Tantal",74:"Tungsten",75:"Reniyum",76:"Osmiyum",77:"İridyum",78:"Platin",79:"Altın",80:"Cıva",
-81:"Talyum",82:"Kurşun",83:"Bizmut",84:"Polonyum",85:"Astatin",86:"Radon",
-87:"Fransiyum",88:"Radyum",89:"Aktinyum",90:"Toryum",91:"Protaktinyum",92:"Uranyum",93:"Neptünyum",94:"Plütonyum",
-95:"Amerikyum",96:"Küriyum",97:"Berkelyum",98:"Kaliforniyum",99:"Einsteinyum",100:"Fermiyum",
-101:"Mendelevyum",102:"Nobelyum",103:"Lavrensiyum",
-104:"Rutherfordyum",105:"Dubniyum",106:"Seaborgiyum",107:"Bohriyum",108:"Hasyum",109:"Meitneryum",
-110:"Darmstadtiyum",111:"Röntgenyum",112:"Kopernikyum",113:"Nihonyum",114:"Flerovyum",
-115:"Moskovyum",116:"Livermoryum",117:"Tennesin",118:"Oganesson"
-};
+/* TÜRKÇE İSİM */
+function turkceIsim(ad){
+    const ceviri = {
+        Hydrogen:"Hidrojen", Helium:"Helyum", Lithium:"Lityum", Beryllium:"Berilyum",
+        Boron:"Bor", Carbon:"Karbon", Nitrogen:"Azot", Oxygen:"Oksijen",
+        Fluorine:"Flor", Neon:"Neon", Sodium:"Sodyum", Magnesium:"Magnezyum",
+        Aluminum:"Alüminyum", Silicon:"Silisyum", Phosphorus:"Fosfor",
+        Sulfur:"Kükürt", Chlorine:"Klor", Argon:"Argon",
+        Potassium:"Potasyum", Calcium:"Kalsiyum", Iron:"Demir",
+        Copper:"Bakır", Silver:"Gümüş", Gold:"Altın",
+        Lead:"Kurşun", Mercury:"Cıva", Uranium:"Uranyum",
+        Plutonium:"Plütonyum", Einsteinium:"Einsteinyum",
+        Fermium:"Fermiyum"
+    };
+    return ceviri[ad] || ad;
+}
+
+/* TÜR */
+function turCevir(kategori){
+    kategori = kategori.toLowerCase();
+
+    if(kategori.includes("metalloid")) return "YarıMetal";
+    if(kategori.includes("noble")) return "Soygaz";
+    if(kategori.includes("metal")) return "Metal";
+
+    return "Ametal";
+}
 
 /* VERİ ÇEK */
 fetch("https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json")
@@ -31,7 +39,7 @@ fetch("https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master
     elementler = veri.elements.map(e => ({
         atomNo: e.number,
         sembol: e.symbol,
-        ad: turkceListe[e.number],
+        ad: turkceIsim(e.name),
         tur: turCevir(e.category),
         atomAgirligi: e.atomic_mass,
         elektron: e.electron_configuration,
@@ -45,18 +53,7 @@ fetch("https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master
     tabloyuCiz(elementler);
 });
 
-/* TÜR BELİRLE */
-function turCevir(kategori){
-    kategori = kategori.toLowerCase();
-
-    if(kategori.includes("metalloid")) return "YarıMetal";
-    if(kategori.includes("noble")) return "Soygaz";
-    if(kategori.includes("metal")) return "Metal";
-
-    return "Ametal";
-}
-
-/* TABLOYU ÇİZ */
+/* TABLO */
 function tabloyuCiz(liste){
 
     tablo.innerHTML = "";
@@ -83,20 +80,16 @@ function tabloyuCiz(liste){
 
 /* MODAL */
 function modalAc(e){
-
     document.getElementById("modal").style.display = "flex";
-
-    document.getElementById("modalName").innerText =
-        `${e.ad} (${e.sembol})`;
-
+    document.getElementById("modalName").innerText = `${e.ad} (${e.sembol})`;
     document.getElementById("modalImage").src = e.gorsel;
 
     document.getElementById("modalDetails").innerHTML = `
         <p><b>Atom No:</b> ${e.atomNo}</p>
-        <p><b>Atom Ağırlığı:</b> ${e.atomAgirligi}</p>
-        <p><b>Elektron Dizilimi:</b> ${e.elektron}</p>
-        <p><b>Erime Noktası:</b> ${e.erime}</p>
-        <p><b>Kaynama Noktası:</b> ${e.kaynama}</p>
+        <p><b>Ağırlık:</b> ${e.atomAgirligi}</p>
+        <p><b>Elektron:</b> ${e.elektron}</p>
+        <p><b>Erime:</b> ${e.erime}</p>
+        <p><b>Kaynama:</b> ${e.kaynama}</p>
         <p><b>Tür:</b> ${e.tur}</p>
     `;
 }
